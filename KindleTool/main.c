b7be007d7894dbd9617672e5b4904c2adc18216e
Yifan Lu <yifanlu94@gmail.com>
2011-10-26 15:03:24 -0500

    Initial Commit

---
+++ KindleTool/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  KindleTool
+//
+//  Created by Yifan Lu on 10/26/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
