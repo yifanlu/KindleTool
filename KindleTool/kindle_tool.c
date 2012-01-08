57f1fd667e14ed50f9406b33d97ab241a5d1e67e
Yifan Lu <yifanlu94@gmail.com>
2012-01-08 13:03:48 -0600

    Add license notice.

--- KindleTool/kindle_tool.c
+++ KindleTool/kindle_tool.c
@@ -2,8 +2,20 @@
 //  main.c
 //  KindleTool
 //
-//  Created by Yifan Lu on 10/26/11.
-//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//  Copyright (C) 2011  Yifan Lu
+//  
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//  
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//  
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //
 
 #include "kindle_tool.h"
